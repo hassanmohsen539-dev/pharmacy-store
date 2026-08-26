8ee53774d04affadb10efa47eb3e8cd74b4a5558
hassanmohsen539-dev <--GitHub>
2026-08-27 00:26:35 +0300

    Remove global auth provider for testing

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AdminNavigation from "./components/AdminNavigation";
 import { LanguageProvider } from "./language-provider";
-import { AuthProvider } from "./auth-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,12 +31,10 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <AuthProvider>
-          <LanguageProvider>
-            <AdminNavigation />
-            {children}
-          </LanguageProvider>
-        </AuthProvider>
+        <LanguageProvider>
+          <AdminNavigation />
+          {children}
+        </LanguageProvider>
       </body>
     </html>
   );
